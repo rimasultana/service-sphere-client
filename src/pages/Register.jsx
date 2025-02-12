aaab9616eaab603060524754ff1e96b1fb5c8f71
rimasultana <rimasultana20225@gmail.com>
2025-02-12 19:39:59 +0600

    remove some text

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -108,7 +108,6 @@ const Register = () => {
               placeholder="Enter your photo URL (optional)"
             />
           </div>
-
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 focus:ring focus:ring-blue-300"
